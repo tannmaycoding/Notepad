ba2783557a13c278fb0241d7b66a411dec531edc
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2024-05-11 17:49:42 +0530

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -1,22 +1,6 @@
 """
 This is a Notepad Using Tkinter.
 Here, you can write text, open files, save files, make new files.
-
-Modules Used:
-    os.path,
-    everything from tkinter,
-    showinfo from tkinter.messagebox,
-    askopenfilename, asksavefilename from tkinter.filedialog
-
-Functions made:
-    save: for saving files,
-    new: for making new files,
-    opening: for opening files,
-    quitApp: for quitting the app,
-    cut: for cutting selected text,
-    copy: for copying selected text,
-    paste: for pasting selected text,
-    about: for opening about page
 """
 
 import os.path
